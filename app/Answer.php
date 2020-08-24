03d561e534ff24f5a521e92a6505b96731aecd3d
MahefaVB <mahefa.ahn@virtualbeehive.com>
2020-08-24 15:54:37 +0200

    Models created
    + eloquent

--- app/Answer.php
+++ app/Answer.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Answer extends Model
 {
-    //
+    public function question(){
+        return $this->belongsTo(Question::class);
+    }
+
+    public function public_user(){
+        return $this->belongsTo(Public_user::class);
+    }
 }
